66dca66107b510259687866ba1a1fff3d1aa7f68
JulienGrade <gradejulien@gmail.com>
2020-03-04 18:58:29 +0100

    Create the page /clients and show customers list,add delete function with react

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,25 +9,33 @@
 
 import React from 'react';
 import ReactDOM from "react-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import { HashRouter, Switch, Route } from "react-router-dom";
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
-import Navbar from "./components/Navbar";
-import HomePage from "./pages/HomePage";
+import CustomersPage from "./pages/CustomersPage";
+
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
 console.log('Hello Julien, tu peux me modifier sur assets/js/app.js');
 
+
+
 const App = () => {
     return (
-        <>
+        <HashRouter>
             <Navbar />
-            <div className="container pt-5">
-                <HomePage/>
-            </div>
-        </>
+            <main className="container pt-5">
+                <Switch>
+                    <Route path="/customers" component={CustomersPage}/>
+                    <Route path="/" component={HomePage}/>
+                </Switch>
+            </main>
+        </HashRouter>
     );
 };
 
---
+++ assets/js/pages/CustomersPage.jsx
@@ -0,0 +1,79 @@
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+
+const CustomersPage = (props) => {
+
+    const [customers, setCustomers] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("http://127.0.0.1:8000/api/clients")
+            .then(response => response.data['hydra:member'])
+            .then(data => setCustomers(data))
+            .catch(error=> console.log(error.response));
+    }, []);
+
+    const handleDelete = (id) => {
+       console.log(id);
+
+       // Ici on enregistre les customers avant la requete,ensuite on efface et si il y
+        // avait une erreur on ré affecte cette valeur a customers
+       const originalCustomers = [...customers];
+       // L'approche optimiste
+        setCustomers(customers.filter(customer => customer.id !== id));
+
+       // L'approche pessimiste
+       axios
+           .delete("http://127.0.0.1:8000/api/customers/" + id)
+           .then(response => console.log("ok"))
+           .catch(error => {
+               setCustomers(originalCustomers);
+               console.log(error.response);
+           });
+    };
+
+    return (
+        <>
+            <h1>Liste des clients</h1>
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Id.</th>
+                        <th>Client</th>
+                        <th>Email</th>
+                        <th>Entreprise</th>
+                        <th className="text-center">Factures</th>
+                        <th className="text-center">Montant total</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {customers.map(customer => (<tr key={customer.id}>
+                        <td>{customer.id}</td>
+                        <td>
+                            <a href="#">{customer.firstName} {customer.lastName}</a>
+                        </td>
+                        <td>{customer.email}</td>
+                        <td>{customer.company}</td>
+                        <td className="text-center">
+                            <span className="badge badge-secondary">{customer.invoices.length}</span>
+                        </td>
+                        <td className="text-center">{customer.totalAmount.toLocaleString()} &euro;</td>
+                        <td>
+                            <button
+                                onClick={()=> handleDelete(customer.id)}
+                                disabled={customer.invoices.length}
+                                className="btn btn-sm btn-danger"
+                            >
+                                Supprimer
+                            </button>
+                        </td>
+                    </tr>))}
+
+                </tbody>
+            </table>
+        </>
+    );
+};
+
+export default CustomersPage;
